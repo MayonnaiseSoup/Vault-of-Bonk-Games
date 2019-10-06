054d9d4d0a30df841b5f894b21318f07a9112c47
Marcus Cornes <familycornes@gmail.com>
2019-10-06 12:17:00 +0800

    initial commit

---
+++ LinuxGaming/main.py
@@ -0,0 +1,23 @@
+while True:
+
+    response = input("What game would you like to play?: ")
+
+
+    if response == "Caterpillar Game":
+        import caterpillargame
+
+
+    if response == "Match Maker":
+        import Matchmaker
+
+
+    if response == "Nine Lives":
+        import NineLives
+
+
+    if response == "exit":
+        break
+
+
+    if response == "Snake":
+        import snake
